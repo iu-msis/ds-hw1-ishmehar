ba2a5e306a926922ca5a8731a32b0e9aebc3d6ba
ishmehar <ichawla@iu.edu>
2018-09-14 16:59:10 -0400

    Data fetched from API dynamically. Button added.

--- public/js/dashboard.app.js
+++ public/js/dashboard.app.js
@@ -1,28 +1,62 @@
 var dashboardapp = new Vue({
   el: '#dashboard',
   data: {
-  "name" : "Tom Jensen",
-  "profile_pic": "",
-  "country_of_origin" : "Ireland",
-  "dob" : "1996-12-22",
-  "email" : "tom.jensen@gmail.com"
+
+info: {
+   "name":{"title":'',"first":'',"last":''},
+   "pictures": '',
+   "location":{city:'',state:''},
+   "dob":{"date":''},
+   "email" : ''
+
+}
 },
 methods: {
 
+  pretty_finaldate: function(d) {
+    return moment(d).format('l')
+  },
+
   pretty_age: function(d) {
     return d;
-  }
-},
+  },
+  fetchTasks () {
+
+     fetch('https://randomuser.me/api')
+
+     .then( response => response.json() )
+     .then( json => {dashboardapp.info = json.results[0]; } )
+     .catch( err => {
+       console.log('TASK FETCH ERROR:');
+       console.log(err);
+
+     });
+}},
+created () {
+
+      this.fetchTasks();
+
+ },
 computed: {
   age_calc: function() {
-     return moment().diff(moment(this.dob),'years')
+    console.log('The issue is here:');
+    console.log('this.dob is:', this.dob);
+    console.log(this.info);
+
+     return moment().diff(moment(this.info.dob.date),'years')
 
 
 },
 pretty_age: function() {
   return this.pretty_age(this.age)
 
+},
+
+pretty_date: function() {
+
+  return this.pretty_date(this.pretty_finaldate)
 }
 
 }
-})
+}
+)
